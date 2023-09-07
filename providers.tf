492987517c8d76d2614f9077b834a9268d4ec078
Ilerioluwa Albert Akinola <ilerioluwaakinola@gmail.com>
2023-09-07 20:12:37 +0200

    Initial Commit

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region                   = "af-south-1"
+  shared_credentials_files = ["C:/Users/Lucy III/.aws/credentials"]
+  profile                  = "vscode"
+}
